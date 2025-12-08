5bcbc98392511fbefca31f9cffa409008c5e356f
RuslanUC <dev_ruslan_uc@protonmail.com>
2025-12-08 12:10:35 +0200

    refactor day 8 a little bit; calculate distances only once

--- day-8.py
+++ day-8.py
@@ -1,47 +1,35 @@
 from math import sqrt
-from typing import Literal, TypeVar, Iterable, Generator
+from typing import TypeVar, Generator
 
 T = TypeVar("T")
-
+CoordT = tuple[int, int, int]
+DistanceT = tuple[int | float, CoordT, CoordT]
+CircuitsT = dict[CoordT, set[CoordT]]
 
 def enumerate_from(it: list[T], start_from: int) -> Generator[tuple[int, T], None, None]:
     for i in range(start_from, len(it)):
         yield i, it[i]
 
 
-def part1(boxes: list[tuple[int, int, int]]) -> None:
-    circuits: dict[int, set[int]] = {}
-
-    distances = [
-        (
-            sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2),
-            idx1,
-            idx2,
-        )
-        for idx1, (x1, y1, z1) in enumerate(boxes)
-        for idx2, (x2, y2, z2) in enumerate_from(boxes, idx1 + 1)
-    ]
-
-    distances.sort(key=lambda e: e[0])
+def part1(distances: list[DistanceT]) -> CircuitsT:
+    circuits: dict[CoordT, set[CoordT]] = {}
 
     for i in range(1000):
-        shortest_two = distances[i][1:]
+        _, xyz1, xyz2 = distances[i]
 
-        i1, i2 = shortest_two
-
-        if i1 not in circuits and i2 not in circuits:
-            circuits[i1] = circuits[i2] = {i1, i2}
-        elif i1 in circuits and i2 not in circuits:
-            circuits[i1].add(i2)
-            circuits[i2] = circuits[i1]
-        elif i1 not in circuits and i2 in circuits:
-            circuits[i2].add(i1)
-            circuits[i1] = circuits[i2]
-        elif circuits[i1] is not circuits[i2]:
-            new_circuit = {*circuits[i1], *circuits[i2]}
-            for j in circuits[i1]:
+        if xyz1 not in circuits and xyz2 not in circuits:
+            circuits[xyz1] = circuits[xyz2] = {xyz1, xyz2}
+        elif xyz1 in circuits and xyz2 not in circuits:
+            circuits[xyz1].add(xyz2)
+            circuits[xyz2] = circuits[xyz1]
+        elif xyz1 not in circuits and xyz2 in circuits:
+            circuits[xyz2].add(xyz1)
+            circuits[xyz1] = circuits[xyz2]
+        elif circuits[xyz1] is not circuits[xyz2]:
+            new_circuit = {*circuits[xyz1], *circuits[xyz2]}
+            for j in circuits[xyz2]:
                 circuits[j] = new_circuit
-            for j in circuits[i2]:
+            for j in circuits[xyz2]:
                 circuits[j] = new_circuit
 
     seen_ids = set()
@@ -50,54 +38,44 @@ def part1(boxes: list[tuple[int, int, int]]) -> None:
     for idx, circuit in enumerate(sorted(circuits.values(), key=len, reverse=True)):
         if id(circuit) in seen_ids:
             continue
-        # print(circuit)
         seen_ids.add(id(circuit))
         if len(seen_ids) <= 3:
             result *= len(circuit)
 
     print(f"Part 1 answer: {result}")
 
-    # print(f"Part 1 multiple of three largest circuits: {result}")
+    return circuits
 
 
-def part2(boxes: list[tuple[int, int, int]]) -> None:
-    circuits: dict[int, set[int]] = {}
+def part2(
+        distances: list[DistanceT], boxes_num: int, circuits: CircuitsT | None = None, start_from: int | None = None
+) -> None:
+    if start_from is None or circuits is None:
+        start_from = 0
+    if circuits is None:
+        circuits = {}
 
-    distances = [
-        (
-            sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2),
-            idx1,
-            idx2,
-        )
-        for idx1, (x1, y1, z1) in enumerate(boxes)
-        for idx2, (x2, y2, z2) in enumerate_from(boxes, idx1 + 1)
-    ]
+    for i in range(start_from, len(distances)):
+        _, xyz1, xyz2 = distances[i]
 
-    distances.sort(key=lambda e: e[0])
-
-    for i in range(len(distances)):
-        shortest_two = distances[i][1:]
-
-        i1, i2 = shortest_two
-
-        if i1 not in circuits and i2 not in circuits:
-            circuits[i1] = circuits[i2] = {i1, i2}
-        elif i1 in circuits and i2 not in circuits:
-            circuits[i1].add(i2)
-            circuits[i2] = circuits[i1]
-        elif i1 not in circuits and i2 in circuits:
-            circuits[i2].add(i1)
-            circuits[i1] = circuits[i2]
-        elif circuits[i1] is not circuits[i2]:
-            new_circuit = {*circuits[i1], *circuits[i2]}
-            for j in circuits[i1]:
+        if xyz1 not in circuits and xyz2 not in circuits:
+            circuits[xyz1] = circuits[xyz2] = {xyz1, xyz2}
+        elif xyz1 in circuits and xyz2 not in circuits:
+            circuits[xyz1].add(xyz2)
+            circuits[xyz2] = circuits[xyz1]
+        elif xyz1 not in circuits and xyz2 in circuits:
+            circuits[xyz2].add(xyz1)
+            circuits[xyz1] = circuits[xyz2]
+        elif circuits[xyz1] is not circuits[xyz2]:
+            new_circuit = {*circuits[xyz1], *circuits[xyz2]}
+            for j in circuits[xyz1]:
                 circuits[j] = new_circuit
-            for j in circuits[i2]:
+            for j in circuits[xyz2]:
                 circuits[j] = new_circuit
 
-        if len(circuits[i1]) == len(boxes):
-            x1, *_ = boxes[i1]
-            x2, *_ = boxes[i2]
+        if len(circuits[xyz1]) == boxes_num:
+            x1 = xyz1[0]
+            x2 = xyz2[0]
             print(f"Part 2 answer: {x1 * x2}")
             break
 
@@ -114,8 +92,20 @@ def main() -> None:
         )
     ]
 
-    part1(boxes)
-    part2(boxes)
+    distances = [
+        (
+            sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2),
+            (x1, y1, z1),
+            (x2, y2, z2),
+        )
+        for idx1, (x1, y1, z1) in enumerate(boxes)
+        for idx2, (x2, y2, z2) in enumerate_from(boxes, idx1 + 1)
+    ]
+
+    distances.sort(key=lambda e: e[0])
+
+    circuits = part1(distances)
+    part2(distances, len(boxes), circuits, 1000)
 
 
 if __name__ == "__main__":
